47a225ed52178224b6eb7188ff43f439348a4550
muhdsaad <mrrmuhammadsaad.com>
2022-01-20 18:35:27 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from './components/Display';
+
+const App = () => {
+  return <div>
+
+      <Display/>
+      
+  </div>;
+};
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Display extends React.Component{
+    constructor(props){
+        super(props)
+      this.state = {display: true};
+      this.toggle = this.toggle.bind(this);
+    }
+    toggle(){
+        ///console.log("button clicked")
+      this.setState(prestate => { return {display: !prestate.display} })
+    }
+    render(){
+        return(
+            <div>
+                {this.state.display && <p>this is a paragraph</p>}
+           <button onClick={this.toggle}>{this.state.display ? "hide" : "show"}</button>
+            </div>
+        )
+    }
+}
+export default Display;
